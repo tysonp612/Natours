595438cbfe2750dfe6a5a5d8ed36045753a2f294
email <phamson061297@gmail.com>
2021-07-12 20:32:41 -0300

    Updated pakage.json

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 //Middleware example
 app.use((req, res, next) => {
-  console.log('Hello from the middleware');
+  // console.log('Hello from the middleware');
   next();
 });
 
